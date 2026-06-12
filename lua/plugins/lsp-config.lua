e9f252681c7081fa3b91a878a76a94ef6f4031f8
Pavel Taranov <pavel.a.taranov@gmail.com>
2026-06-13 00:49:24 +0300

    Started LSP support - incomplete!

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,18 @@
+-- Portable package manager for Neovim that runs everywhere Neovim runs.
+-- Easily install and manage LSP servers, DAP servers, linters, and formatters.
+return {
+        "mason-org/mason.nvim",
+--      version = "v2.3.1"
+
+        -- fc98833b6da5de5a9c5b1446ac541577059555be
+        version = "v1.11.0",
+
+        opts = {
+        }
+}
+-- "williamboman/mason.nvim"
+--      opts = {}
+--      config =  function()
+--              require("mason").setup({
+--              })
+--      end
